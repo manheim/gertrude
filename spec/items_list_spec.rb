7e142419c4e615bce285e91af5bc68c968729817
Wallace Harwood <wallace.harwood@manheim.com>
2016-04-01 12:49:36 -0500

    [wallace] removing legacy spec file

--- spec/items_list_spec.rb
+++ spec/items_list_spec.rb
@@ -131,4 +131,33 @@ describe 'items list' do
       expect(item_list.get_available_item('admin')).to eql({"johnny5" => {"user_name" => "danny7", "rep_id" => "100014624", "profile_id" => "8192", ItemsList::RESERVE_KEY.to_sym => false}})
     end
   end
+
+  describe '#load_items!' do
+    let(:hash) { {type: {test: {hello: 'world', foo: 'bar'}, basic: {basic1: {}, basic2: {}}}} }
+    let(:duplicate_hash) { {type: {test: {hello: 'world', foo: 'bar'}}, type2: {test: {hello: 'world'}}} }
+
+    it 'should add a unique key 2nd level to the given hash' do
+      allow(YAML).to receive(:load_file).with('').and_return(hash)
+      expect(ItemsList.new.load_items!('').items[:type][:test].keys).to include ItemsList::RESERVE_KEY
+    end
+
+    it 'should set @items to a hash' do
+      allow(YAML).to receive(:load_file).with('').and_return(hash)
+      items = ItemsList.new
+      items.load_items!('')
+      expect(items.instance_variable_get('@items')).to be_a_kind_of Hash
+    end
+
+    it 'should raise an error if duplicate keys' do
+      allow(YAML).to receive(:load_file).with('').and_return(duplicate_hash)
+      items = ItemsList.new
+      expect { items.load_items!('') }.to raise_error ItemError::ItemsNotUnique
+    end
+  end
+
+    describe '#load_items!' do
+      it 'should raise an error on invalid config file' do
+        expect { ItemsList.new.load_items!('blah123') }.to raise_error(Errno::ENOENT)
+      end
+    end
 end
