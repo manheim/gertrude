439a4f692c96aee46777be1a790a55023dc08304
Simeon <simeonberns@gmail.com>
2017-01-19 14:51:01 -0600

    Added methods from active_support to do stringify keys

--- lib/gertrude/core_ext/hash.rb
+++ lib/gertrude/core_ext/hash.rb
@@ -14,4 +14,28 @@ class Hash
     hash.find { |*x| found = deep_find(key, x.last) }
     found
   end
+
+  def deep_stringify_keys!
+    deep_transform_keys!(&:to_s)
+  end
+
+  def deep_transform_keys!(&block)
+    _deep_transform_keys_in_object!(self, &block)
+  end
+
+  private
+  def _deep_transform_keys_in_object!(object, &block)
+    case object
+      when Hash
+        object.keys.each do |key|
+          value = object.delete(key)
+          object[yield(key)] = _deep_transform_keys_in_object!(value, &block)
+        end
+        object
+      when Array
+        object.map! {|e| _deep_transform_keys_in_object!(e, &block)}
+      else
+        object
+    end
+  end
 end
